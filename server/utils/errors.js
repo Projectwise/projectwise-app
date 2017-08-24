49964f412c6917980e69b8dc4e14794571c270af
Yogesh Kumar <yogeshkumar180592@gmail.com>
2017-08-25 02:31:48 +0530

    Project restructuring

--- server/utils/errors.js
+++
@@ -1,9 +0,0 @@
-
-const invalidDataError = (statusCode, details) => {
-  const err = new Error('Invalid Data')
-  err.statusCode = statusCode
-  err.details = details
-  return err
-}
-
-exports.invalidDataError = invalidDataError
